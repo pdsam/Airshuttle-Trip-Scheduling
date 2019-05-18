b8e98431c067814d66a3314e6b695ca416971ccd
GambuzX <mg_mesquita@hotmail.com>
2019-05-18 16:06:57 +0100

    Created mother class for GraphAlgorithms

---
+++ src/Algorithms/GraphAlgorithm.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_ALGORITHM_H_
+#define GRAPH_ALGORITHM_H_
+
+#include "../Graph/Graph.h"
+
+class GraphAlgorithm {
+protected:
+	Graph * graph;
+
+public:
+	GraphAlgorithm(Graph * graph);
+	virtual ~GraphAlgorithm();
+
+	virtual void execute(Vertex * start) = 0;
+
+};
+
+#endif
