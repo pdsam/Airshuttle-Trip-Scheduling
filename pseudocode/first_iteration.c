315666aa2af21da27f97db7c4decc80fb8968ce0
GambuzX <mg_mesquita@hotmail.com>
2019-05-26 18:16:34 +0100

    DFS pseudocode

--- pseudocode/first_iteration.c
+++ pseudocode/first_iteration.c
@@ -10,13 +10,13 @@ PLAN_SINGLE_VAN_NOT_MIXING_PASSENGERS(Gi, Ri, v):
 
     for each r ∈ Ri do
         // Get path to destination and back to airport
-        path_edges = GET_PATH_EDGES(Gi, a, DEST(r))
+        path_edges = GET_PATH_EDGES(Gi, a, dest(r))
         return_path = REVERSE_PATH(path_edges)
         path_edges = JOIN_PATHS(path_edges, return_path)
 
         // Create and add new service to the van
         // SERVICE(vacant, start_time, reservation, path)
-        new_service = SERVICE(VAN_CAPACITY(v) - NUM_PEOPLE(r), LAST_SERVICE(v), r, path_edges)
+        new_service = SERVICE(van_capacity(v) - num_people(r), LAST_SERVICE_END(v), r, path_edges)
         ADD_SERVICE(v, new_service)
 
         
--- pseudocode/pre_processing.c
+++ pseudocode/pre_processing.c
@@ -2,11 +2,10 @@
 // Ri - sequence of reservations
 // a - airport
 PRE_PROCESS_DATA(Gi, Ri, a):
-    DFS_VISIT(Gi, a)
+    DFS_CONNECTIVITY(Gi, a)
     REMOVE_UNVISITED_VERTICES(Gi)
 
     for each r ∈ Ri do
-        if FIND_VERTEX(Gi, DEST(r)) == NULL then
+        if FIND_VERTEX(Gi, dest(r)) == NULL then
             REMOVE_RESERVATION(Ri, r)
 
-            
